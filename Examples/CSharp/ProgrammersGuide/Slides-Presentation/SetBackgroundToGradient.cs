d79db017b2c5cddc0360cf10bf240be73ccec4d6
Nausherwan Aslam <nausherwan.aslam@aspose.com>
2015-08-24 22:04:20 +0500

    Updated ReadMe and Copyrights message

--- Examples/CSharp/ProgrammersGuide/Slides-Presentation/SetBackgroundToGradient.cs
+++ Examples/CSharp/ProgrammersGuide/Slides-Presentation/SetBackgroundToGradient.cs
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////
-// Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+// Copyright 2001-2015 Aspose Pty Ltd. All Rights Reserved.
 //
 // This file is part of Aspose.Slides. The source code in this file
 // is only intended as a supplement to the documentation, and is provided
