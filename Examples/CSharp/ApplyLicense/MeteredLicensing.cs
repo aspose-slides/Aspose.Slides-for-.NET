c65713bb1ad94a56b41899518072d331b021b46a
Muhammad-Adnan-Ahmad <adnan.ahmad@aspose.com>
2017-12-21 21:36:01 +0500

    Added Metered License Example
    MeteredLicensing.cs

---
+++ Examples/CSharp/ApplyLicense/MeteredLicensing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.CAD.Examples.CSharp.ApplyLicense
+{
+    class MeteredLicensing
+    {
+
+        public static void Run()
+        {
+            //ExStart:MeteredLicensing
+
+            // Create an instance of CAD Metered class
+            Aspose.Slides.Metered metered = new Aspose.Slides.Metered();
+
+            // Access the setMeteredKey property and pass public and private keys as parameters
+            metered.SetMeteredKey("*****", "*****");
+
+            // Get metered data amount before calling API
+            decimal amountbefore = Aspose.Slides.Metered.GetConsumptionQuantity();
+
+            // Display information
+            Console.WriteLine("Amount Consumed Before: " + amountbefore.ToString());
+            // Get metered data amount After calling API
+            decimal amountafter = Aspose.Slides.Metered.GetConsumptionQuantity();
+
+            // Display information
+            Console.WriteLine("Amount Consumed After: " + amountafter.ToString());
+
+            //ExEnd:MeteredLicensing
+        }
+
+    }
+}
