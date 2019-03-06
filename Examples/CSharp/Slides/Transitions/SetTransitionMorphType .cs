41a31905439352ac4ea90c317998e8c997430761
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-03-06 23:48:01 +0500

    Added examples based on 19.2

---
+++ Examples/CSharp/Slides/Transitions/SetTransitionMorphType .cs
@@ -0,0 +1,30 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using Aspose.Slides.SlideShow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Slides.Transitions
+{
+    class SetTransitionMorphType
+    {
+        public static void Run() {
+
+            //ExStart:SetTransitionMorphType
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Slides_Presentations_Transitions();
+
+            using (Presentation presentation = new Presentation(dataDir + "presentation.pptx"))
+            {
+                presentation.Slides[0].SlideShowTransition.Type = TransitionType.Morph;
+                ((IMorphTransition)presentation.Slides[0].SlideShowTransition.Value).MorphType = TransitionMorphType.ByWord;
+                presentation.Save(dataDir + "presentation-out.pptx", SaveFormat.Pptx);
+            }
+            //ExEnd:SetTransitionMorphType
+        }
+    }
+}
---
+++ Examples/CSharp/Slides/Transitions/SupportOfMorphTransition.cs
@@ -0,0 +1,40 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Slides.Transitions
+{
+    class SupportOfMorphTransition
+    {
+        public static void Run()
+        {
+            //ExStart:SupportOfMorphTransition
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Slides_Presentations_Transitions();
+
+            using (Presentation presentation = new Presentation())
+            {
+                AutoShape autoshape = (AutoShape)presentation.Slides[0].Shapes.AddAutoShape(ShapeType.Rectangle, 100, 100, 400, 100);
+                autoshape.TextFrame.Text = "Test text";
+
+                presentation.Slides.AddClone(presentation.Slides[0]);
+
+                presentation.Slides[1].Shapes[0].X += 100;
+                presentation.Slides[1].Shapes[0].Y += 50;
+                presentation.Slides[1].Shapes[0].Width -= 200;
+                presentation.Slides[1].Shapes[0].Height -= 10;
+
+                presentation.Slides[1].SlideShowTransition.Type = Aspose.Slides.SlideShow.TransitionType.Morph;
+
+                presentation.Save(dataDir+"presentation-out.pptx", SaveFormat.Pptx);
+            }
+
+            //ExEnd:SupportOfMorphTransition
+        }
+    }
+}
