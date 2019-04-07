81a2666f186933edefeb5d7eb10f90f98b6284b2
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-04-08 00:28:01 +0500

    Added new Example

---
+++ Examples/CSharp/Presentations/Conversion/ExportToHTMLWithResponsiveLayout.cs
@@ -0,0 +1,27 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Presentations.Conversion
+{
+    class ExportToHTMLWithResponsiveLayout
+    {
+        public static void Run() {
+
+            //ExStart:ExportToHTMLWithResponsiveLayout
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Conversion();
+
+            Presentation presentation = new Presentation(dataDir+"SomePresentation.pptx");
+            HtmlOptions saveOptions = new HtmlOptions();
+            saveOptions.SvgResponsiveLayout = true;
+            presentation.Save(dataDir+"SomePresentation-out.html", SaveFormat.Html, saveOptions);
+            //ExEnd:ExportToHTMLWithResponsiveLayout
+        }
+    }
+}
