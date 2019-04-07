1c5995476acb62ad7418b3f99334091222036717
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-04-08 00:32:16 +0500

    Added examples based on 19.3

---
+++ Examples/CSharp/Text/HighlightText.cs
@@ -0,0 +1,32 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Text
+{
+    class HighlightText
+    {
+        public static void Run()
+        {
+
+            //ExStart:HighlightText
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Text();
+            Presentation presentation = new Presentation(dataDir +"SomePresentation.pptx");
+            ((AutoShape)presentation.Slides[0].Shapes[0]).TextFrame.HighlightText("title", Color.LightBlue); // highlighting all words 'important'
+            ((AutoShape)presentation.Slides[0].Shapes[0]).TextFrame.HighlightText("to", Color.Violet, new TextHighlightingOptions()
+            {
+                WholeWordsOnly = true
+            }); // highlighting all separate 'the' occurrences
+            presentation.Save(dataDir+ "SomePresentation-out2.pptx", SaveFormat.Pptx);
+
+            //ExEnd:HighlightText
+        }
+    }
+}
---
+++ Examples/CSharp/Text/HighlightTextusingRegex.cs
@@ -0,0 +1,28 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Text
+{
+    class HighlightTextUsingRegx
+    {
+        public static void Run() {
+
+            //ExStart:HighlightTextUsingRegx
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Text();
+            Presentation presentation = new Presentation(dataDir + "SomePresentation.pptx");
+            TextHighlightingOptions options = new TextHighlightingOptions();
+            ((AutoShape)presentation.Slides[0].Shapes[0]).TextFrame.HighlightRegex(@"\b[^\s]{5,}\b", Color.Blue, options); // highlighting all words with 10 symbols or longer
+            presentation.Save(dataDir+ "SomePresentation-out.pptx", SaveFormat.Pptx);
+
+            //ExEnd:HighlightTextUsingRegx
+        }
+    }
+}
