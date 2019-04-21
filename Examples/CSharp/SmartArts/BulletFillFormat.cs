2822838e57ee167c5e43b0bac2d26e7f649c1491
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-04-21 12:37:43 +0500

    Added new example

---
+++ Examples/CSharp/SmartArts/BulletFillFormat.cs
@@ -0,0 +1,40 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using Aspose.Slides.SmartArt;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.SmartArts
+{
+    class BulletFillFormat
+    {
+        public static void Run() {
+
+            //ExStart:BulletFillFormat
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_SmartArts();
+
+            using (Presentation presentation = new Presentation())
+            {
+                ISmartArt smart = presentation.Slides[0].Shapes.AddSmartArt(10, 10, 500, 400, SmartArtLayoutType.VerticalPictureList);
+                ISmartArtNode node = smart.AllNodes[0];
+
+                if (node.BulletFillFormat != null)
+                {
+                    Image img = (Image)new Bitmap(dataDir + "aspose-logo.jpg");
+                    IPPImage image = presentation.Images.AddImage(img);
+                    node.BulletFillFormat.FillType = FillType.Picture;
+                    node.BulletFillFormat.PictureFillFormat.Picture.Image = image;
+                    node.BulletFillFormat.PictureFillFormat.PictureFillMode = PictureFillMode.Stretch;
+                }
+                presentation.Save(dataDir +"out.pptx", SaveFormat.Pptx);
+            }
+            //ExEnd:BulletFillFormat
+        }
+    }
+}
