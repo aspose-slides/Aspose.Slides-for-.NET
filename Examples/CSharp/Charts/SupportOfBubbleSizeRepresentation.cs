0cff1fd844772b9c26028f21b481199e4654c349
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-06-03 01:38:41 +0500

    Added example w.r.t 19.5

---
+++ Examples/CSharp/Charts/SupportOfBubbleSizeRepresentation.cs
@@ -0,0 +1,36 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Charts;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Charts
+{
+    class SupportOfBubbleSizeRepresentation
+    {
+
+        public static void Run() {
+
+
+            //ExStart:SupportOfBubbleSizeRepresentation
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Charts();
+
+            using (Presentation pres = new Presentation())
+            {
+                IChart chart = pres.Slides[0].Shapes.AddChart(ChartType.Bubble, 50, 50, 600, 400, true);
+
+                chart.ChartData.SeriesGroups[0].BubbleSizeRepresentation = BubbleSizeRepresentationType.Width;
+
+                pres.Save(dataDir+ "Presentation_BubbleSizeRepresentation.pptx", SaveFormat.Pptx);
+            }
+
+            //ExEnd:SupportOfBubbleSizeRepresentation
+
+        }
+    }
+}
