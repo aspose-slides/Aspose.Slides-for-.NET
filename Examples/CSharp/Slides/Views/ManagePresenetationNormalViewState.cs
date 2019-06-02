ec1e24096d54c55bb3945a1254426efc1e9b20fa
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-06-03 01:41:04 +0500

    Added example w.r.t 19.5

---
+++ Examples/CSharp/Slides/Views/ManagePresenetationNormalViewState.cs
@@ -0,0 +1,35 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Slides.Views
+{
+    class ManagePresenetationNormalViewState
+    {
+        public static void Run() {
+
+            //ExStart:ManagePresenetationNormalViewState
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Slides_Presentations_Views();
+
+            using (Presentation pres = new Presentation())
+            {
+                pres.ViewProperties.NormalViewProperties.HorizontalBarState = SplitterBarStateType.Restored;
+                pres.ViewProperties.NormalViewProperties.VerticalBarState = SplitterBarStateType.Maximized;
+
+                pres.ViewProperties.NormalViewProperties.RestoredTop.AutoAdjust = true;
+                pres.ViewProperties.NormalViewProperties.RestoredTop.DimensionSize = 80;
+                pres.ViewProperties.NormalViewProperties.ShowOutlineIcons = true;
+
+                pres.Save(dataDir+ "presentation_normal_view_state.pptx", SaveFormat.Pptx);
+            }
+
+            //ExEnd:ManagePresenetationNormalViewState
+        }
+    }
+}
