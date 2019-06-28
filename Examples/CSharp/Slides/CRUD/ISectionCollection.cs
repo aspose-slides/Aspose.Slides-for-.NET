ff27d90b613e44f119c4edb9d582460637b9ce30
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-06-28 15:38:56 +0500

    Updated directory path

--- Examples/CSharp/Slides/CRUD/ISectionCollection.cs
+++ Examples/CSharp/Slides/CRUD/ISectionCollection.cs
@@ -18,14 +18,14 @@ namespace Aspose.Slides.Examples.CSharp.Slides
             // The path to the documents directory.
             string dataDir = RunExamples.GetDataDir_Slides_Presentations_CRUD();
 
-           Presentation pres = new Presentation(path+"Presentation1.pptx");
+           Presentation pres = new Presentation(dataDir + "Presentation1.pptx");
             ISection section = pres.Sections[2];
             pres.Sections.ReorderSectionWithSlides(section, 0);
             pres.Sections.RemoveSectionWithSlides(pres.Sections[0]);
             pres.Sections.AppendEmptySection("Last empty section");
             pres.Sections.AddSection("First empty", pres.Slides[0]);
             pres.Sections[0].Name = "New section name";
-            pres.Save(path+"resultsection1.pptx",Aspose.Slides.Export.SaveFormat.Pptx);
+            pres.Save(dataDir + "resultsection1.pptx",Aspose.Slides.Export.SaveFormat.Pptx);
             }
             //ExEnd:ISectionCollection
         }
