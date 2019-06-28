2e01e7f6adedae2e4197f483fc6b5b1ddf735b75
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-06-28 15:37:44 +0500

    Added example w.r.t 19.6

---
+++ Examples/CSharp/Slides/Background/GetBackgroundEffectiveValues.cs
@@ -0,0 +1,34 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Slides.Background
+{
+    class GetBackgroundEffectiveValues
+    {
+
+        public static void Run()
+        {
+            //ExStart:GetBackgroundEffectiveValues
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Slides_Presentations_Background();
+
+            // Instantiate the Presentation class that represents the presentation file
+            Presentation pres = new Presentation(dataDir + "SamplePresentation.pptx");
+
+            IBackgroundEffectiveData effBackground = pres.Slides[0].CreateBackgroundEffective();
+
+            if (effBackground.FillFormat.FillType == FillType.Solid)
+                Console.WriteLine("Fill color: " + effBackground.FillFormat.SolidFillColor);
+            else
+                Console.WriteLine("Fill type: " + effBackground.FillFormat.FillType);
+
+            //ExEnd:GetBackgroundEffectiveValues
+        }
+
+    }
+}
