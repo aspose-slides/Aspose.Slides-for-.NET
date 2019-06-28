9294833e102e9b60d8e04d7b336cfaa280e46709
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-06-28 15:33:55 +0500

    Added example w.r.t 19.6

---
+++ Examples/CSharp/Presentations/Saving/AddBlobImageToPresentation.cs
@@ -0,0 +1,44 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Presentations.Saving
+{
+    class AddBlobImageToPresentation
+    {
+        public static void Run() {
+
+            //ExStart:AddBlobImageToPresentation
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PresentationSaving();
+
+            string pathToLargeImage = dataDir + "large_image.jpg";
+
+            // create a new presentation which will contain this image
+            using (Presentation pres = new Presentation())
+            {
+                using (FileStream fileStream = new FileStream(pathToLargeImage, FileMode.Open))
+                {
+                    // let's add the image to the presentation - we choose KeepLocked behavior, because we not
+                    // have an intent to access the "largeImage.png" file.
+                    IPPImage img = pres.Images.AddImage(fileStream, LoadingStreamBehavior.KeepLocked);
+                    pres.Slides[0].Shapes.AddPictureFrame(ShapeType.Rectangle, 0, 0, 300, 200, img);
+
+                    // save the presentation. Despite that the output presentation will be
+                    // large, the memory consumption will be low the whole lifetime of the pres object
+                    pres.Save(dataDir + "presentationWithLargeImage.pptx", SaveFormat.Pptx);
+                }
+            }
+
+            //ExEnd:AddBlobImageToPresentation
+
+        }
+    }
+}
