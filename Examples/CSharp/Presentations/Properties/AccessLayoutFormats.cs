9619608a7aaf5c8cc6a75f632ddab6ced33876e0
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-08-01 09:45:25 +0500

    Added example w.r.t 19.7

---
+++ Examples/CSharp/Presentations/Properties/AccessLayoutFormats.cs
@@ -0,0 +1,33 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Presentations.Properties
+{
+    class AccessLayoutFormats
+    {
+        public static void Run() {
+
+            //ExStart:AccessLayoutFormats
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_PresentationProperties();
+
+            using (Presentation pres = new Presentation(dataDir + "pres.pptx"))
+            {
+                foreach (ILayoutSlide layoutSlide in pres.LayoutSlides)
+                {
+                    IFillFormat[] fillFormats = layoutSlide.Shapes.Select(shape => shape.FillFormat).ToArray();
+                    ILineFormat[] lineFormats = layoutSlide.Shapes.Select(shape => shape.LineFormat).ToArray();
+                }
+            }
+            //ExEnd:AccessLayoutFormats
+
+        }
+
+    }
+}
