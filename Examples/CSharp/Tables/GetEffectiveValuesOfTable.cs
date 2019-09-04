53e81d0d0cc9873a3dd24443b3f0ed86b37718e5
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-09-04 16:18:39 +0500

    Added example w.r.t 19.8

---
+++ Examples/CSharp/Tables/GetEffectiveValuesOfTable.cs
@@ -0,0 +1,38 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Tables
+{
+    class GetEffectiveValuesOfTable
+    {
+        public static void Run() {
+
+            //ExStart:GetEffectiveValuesOfTable
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Tables();
+
+            using (Presentation pres = new Presentation(dataDir + "pres.pptx"))
+            {
+                ITable tbl = pres.Slides[0].Shapes[0] as ITable;
+                ITableFormatEffectiveData tableFormatEffective = tbl.TableFormat.GetEffective();
+                IRowFormatEffectiveData rowFormatEffective = tbl.Rows[0].RowFormat.GetEffective();
+                IColumnFormatEffectiveData columnFormatEffective = tbl.Columns[0].ColumnFormat.GetEffective();
+                ICellFormatEffectiveData cellFormatEffective = tbl[0, 0].CellFormat.GetEffective();
+
+                IFillFormatEffectiveData tableFillFormatEffective = tableFormatEffective.FillFormat;
+                IFillFormatEffectiveData rowFillFormatEffective = rowFormatEffective.FillFormat;
+                IFillFormatEffectiveData columnFillFormatEffective = columnFormatEffective.FillFormat;
+                IFillFormatEffectiveData cellFillFormatEffective = cellFormatEffective.FillFormat;
+               
+            }
+            //ExEnd:GetEffectiveValuesOfTable
+
+        }
+    }
+}
