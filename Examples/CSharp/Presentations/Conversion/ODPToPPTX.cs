3317cf8e476f855e3a168d8cb822bebb6bd960d5
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2019-10-18 09:28:07 +0500

    Added new example

---
+++ Examples/CSharp/Presentations/Conversion/ODPToPPTX.cs
@@ -0,0 +1,37 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Presentations.Conversion
+{
+    class ODPToPPTX
+    {
+
+        public static void Run() {
+
+            //ExStart:ODPToPPTX
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Conversion();
+
+            
+            string srcFileName = dataDir + "AccessOpenDoc.odp";
+            string destFileName = dataDir + "AccessOpenDoc.pptx";
+            //Instantiate a Presentation object that represents a presentation file
+            using (Presentation pres = new Presentation(srcFileName))
+            {
+                //Saving the PPTX presentation to PPTX format
+                pres.Save(destFileName, SaveFormat.Pptx);
+            }
+            //ExEnd:ODPToPPTX
+
+
+
+        }
+    }
+}
