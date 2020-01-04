42e42ae1053fc1fcad66e37970f239210ebb2fc0
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2020-01-04 23:15:49 +0500

    Added example w.r.t 19.12

---
+++ Examples/CSharp/Rendering-Printing/SupportOfDigitalSignature.cs
@@ -0,0 +1,42 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Rendering_Printing
+{
+    class SupportOfDigitalSignature
+    {
+
+        public static void Run() {
+
+            //ExStart:SupportOfDigitalSignature
+
+            string dataDir = RunExamples.GetDataDir_Rendering();
+
+            using (Presentation pres = new Presentation())
+            {
+                
+                DigitalSignature signature = new DigitalSignature(dataDir + "testsignature1.pfx", @"testpass1");
+
+                
+                signature.Comments = "Aspose.Slides digital signing test.";
+
+                
+                pres.DigitalSignatures.Add(signature);
+
+                
+                pres.Save(dataDir + "SomePresentationSigned.pptx", SaveFormat.Pptx);
+            }
+
+            //ExEnd:SupportOfDigitalSignature
+
+
+
+        }
+    }
+}
