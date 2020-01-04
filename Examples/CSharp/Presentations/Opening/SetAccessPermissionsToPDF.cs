1b2cb345167d86e5301d7da2288a27b233d83b96
mannanfazil <46891662+mannanfazil@users.noreply.github.com>
2020-01-04 23:13:03 +0500

    Added example w.r.t 9.12

---
+++ Examples/CSharp/Presentations/Opening/SetAccessPermissionsToPDF.cs
@@ -0,0 +1,34 @@
+﻿using Aspose.Slides;
+using Aspose.Slides.Examples.CSharp;
+using Aspose.Slides.Export;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Presentations.Opening
+{
+    class SetAccessPermissionsToPDF
+    {
+
+        public static void Run() {
+
+            //ExStart:SetAccessPermissionsToPDF
+
+            string dataDir = RunExamples.GetDataDir_PresentationOpening();
+
+            var pdfOptions = new PdfOptions();
+            pdfOptions.Password = "my_password";
+            pdfOptions.AccessPermissions = PdfAccessPermissions.PrintDocument | PdfAccessPermissions.HighQualityPrint;
+
+            using (var presentation = new Presentation())
+            {
+                presentation.Save(dataDir + "PDFWithPermissions.pdf", SaveFormat.Pdf, pdfOptions);
+            }
+            //ExEnd:SetAccessPermissionsToPDF
+
+
+        }
+    }
+}
